a6cb7c3a006eecad7f035899327c619668f9e297
dasschnee <jon.schnaecker@gmail.com>
2021-05-24 17:48:27 +0200

    test

--- mmdet/datasets/test.py
+++ mmdet/datasets/test.py
@@ -3,44 +3,45 @@ import numpy as np
 
 from .builder import DATASETS
 from .custom import CustomDataset
+from .coco import CocoDataset
 
 
 @DATASETS.register_module()
-class TestDataset(CustomDataset):
+class TestDataset(CocoDataset):
 
     CLASSES = ('leg',)
     
-    def load_annotations(self, ann_file):
-        ann_list = mmcv.list_from_file(ann_file)
-        # print('ann_list', ann_list)
-        data_infos = []
-        for i, ann_line in enumerate(ann_list):
-            if ann_line != '#':
-                continue
+    # def load_annotations(self, ann_file):
+    #     ann_list = mmcv.list_from_file(ann_file)
+    #     # print('ann_list', ann_list)
+    #     data_infos = []
+    #     for i, ann_line in enumerate(ann_list):
+    #         if ann_line != '#':
+    #             continue
 
-            img_shape = ann_list[i + 2].split(' ')
-            width = int(img_shape[0])
-            height = int(img_shape[1])
-            bbox_number = int(ann_list[i + 3])
+    #         img_shape = ann_list[i + 2].split(' ')
+    #         width = int(img_shape[0])
+    #         height = int(img_shape[1])
+    #         bbox_number = int(ann_list[i + 3])
 
-            anns = ann_line.split(' ')
-            bboxes = []
-            labels = []
-            for anns in ann_list[i + 4:i + 4 + bbox_number]:
-                bboxes.append([float(ann) for ann in anns[:4]])
-                labels.append(int(anns[4]))
-            print('bbox', *bboxes)
-            data_infos.append(
-                dict(
-                    filename=ann_list[i + 1],
-                    width=width,
-                    height=height,
-                    ann=dict(
-                        bboxes=np.array(bboxes).astype(np.float32),
-                        labels=np.array(labels).astype(np.int64))
-                ))
-        print('data_infos', *data_infos, sep = ", ")
-        return data_infos
+    #         anns = ann_line.split(' ')
+    #         bboxes = []
+    #         labels = []
+    #         for anns in ann_list[i + 4:i + 4 + bbox_number]:
+    #             bboxes.append([float(ann) for ann in anns[:4]])
+    #             labels.append(int(anns[4]))
+    #         print('bbox', *bboxes)
+    #         data_infos.append(
+    #             dict(
+    #                 filename=ann_list[i + 1],
+    #                 width=width,
+    #                 height=height,
+    #                 ann=dict(
+    #                     bboxes=np.array(bboxes).astype(np.float32),
+    #                     labels=np.array(labels).astype(np.int64))
+    #             ))
+    #     print('data_infos', *data_infos, sep = ", ")
+    #     return data_infos
 
-    def get_ann_info(self, idx):
-        return self.data_infos[idx]['ann']
+    # def get_ann_info(self, idx):
+    #     return self.data_infos[idx]['ann']
