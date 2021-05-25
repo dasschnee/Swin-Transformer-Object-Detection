222d28a387ff6416be33a8c60402260caf4cbda6
dasschnee <jon.schnaecker@gmail.com>
2021-05-25 18:38:23 +0200

    changed load_from

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -11,6 +11,6 @@ custom_hooks = [dict(type='NumClassCheckHook')]
 
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = None
+load_from = './work_dirs/cascade_rcnn_swin_tiny_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco/latest.pth'
 resume_from = None
 workflow = [('train', 1)]
